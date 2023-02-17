677164dddfc7e908f7daa8eed72c172e70678a27
zykscode <111462249+zykscode@users.noreply.github.com>
2023-02-17 22:38:14 +0100

    feat:state colour

---
+++ data/makeup.ts
@@ -0,0 +1,182 @@
+export const makeup = {
+  2023: {
+    senate: {
+      partyMakeup: {
+        APC: {
+          seats: 63,
+        },
+        PDP: {
+          seats: 44,
+        },
+        YPP: {
+          seats: 1,
+        },
+      },
+      genderMakeUp: {
+        male: 102,
+        female: 7,
+      },
+    },
+    rep: {
+      partyMakeup: {
+        APC: {
+          seats: 210,
+        },
+        PDP: {
+          seats: 132,
+        },
+        APGA: {
+          seats: 9,
+        },
+        AA: {
+          seats: 2,
+        },
+        ADC: {
+          seats: 2,
+        },
+        AP: {
+          seats: 1,
+        },
+        ADP: {
+          seats: 1,
+        },
+        APM: {
+          seats: 1,
+        },
+        LP: {
+          seats: 1,
+        },
+        SDP: {
+          seats: 1,
+        },
+      },
+      genderMakeUp: {
+        male: 347,
+        female: 13,
+      },
+    },
+  },
+  2019: {
+    senate: {
+      partyMakeup: {
+        APC: {
+          seats: 64,
+        },
+        PDP: {
+          seats: 44,
+        },
+        YPP: {
+          seats: 1,
+        },
+      },
+      genderMakeUp: {
+        male: 102,
+        female: 7,
+      },
+    },
+    rep: {
+      partyMakeup: {
+        APC: {
+          seats: 210,
+        },
+        PDP: {
+          seats: 132,
+        },
+        APGA: {
+          seats: 9,
+        },
+        AA: {
+          seats: 2,
+        },
+        ADC: {
+          seats: 2,
+        },
+        AP: {
+          seats: 1,
+        },
+        ADP: {
+          seats: 1,
+        },
+        APM: {
+          seats: 1,
+        },
+        LP: {
+          seats: 1,
+        },
+        SDP: {
+          seats: 1,
+        },
+      },
+      genderMakeUp: {
+        male: 347,
+        female: 13,
+      },
+    },
+  },
+  2015: {
+    senate: {
+      partyMakeup: {
+        APC: {
+          seats: 63,
+        },
+        PDP: {
+          seats: 44,
+        },
+        YPP: {
+          seats: 1,
+        },
+      },
+      genderMakeUp: {
+        male: 102,
+        female: 7,
+      },
+    },
+    rep: {
+      partyMakeup: {
+        APC: {
+          seats: 212,
+        },
+        PDP: {
+          seats: 140,
+        },
+        APGA: {
+          seats: 5,
+        },
+        AP: {
+          seats: 1,
+        },
+        LP: {
+          seats: 1,
+        },
+      },
+      genderMakeUp: {
+        male: 340,
+        female: 20,
+      },
+    },
+  },
+};
+
+export const demograph = {
+  gender: {
+    male: 49054162,
+    female: 44414846,
+  },
+  occupation: {
+    artisan: 4967464,
+    farming: 14742554,
+    publicServant: 2376223,
+    student: 26027481,
+    houseWife: 13006939,
+    businessman: 12457997,
+    others: 6608143,
+    trading: 7998658,
+    civilServant: 5283549,
+  },
+  age: {
+    youth: 37060339,
+    middleAge: 33413591,
+    elderly: 17700270,
+    old: 5294748,
+  },
+};
